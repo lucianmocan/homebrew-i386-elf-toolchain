2235b971e962972bf187f1a895480c870d02a1f9
Lucian Mocan <62904583+lucianmocan@users.noreply.github.com>
2024-10-01 10:09:20 +0200

    Update i386-elf-gcc.rb sha256

--- Formula/i386-elf-gcc.rb
+++ Formula/i386-elf-gcc.rb
@@ -2,7 +2,7 @@ class I386ElfGcc < Formula
   desc "GNU Compiler Collection targetting i386-elf"
   homepage "https://gcc.gnu.org"
   url "https://mirror.linux-ia64.org/gnu/gcc/gcc-14.1.0/gcc-14.1.0.tar.xz"
-  sha512 "0c6ee313c336ebb028aac17ef60bac2dcf0b285f38c99916164c091506c9d56dedf60a96342de1e91e7c4eaf70d936cc0099c850dd5f148f425b05efb30f83eb"
+  sha256 "9cf7cb363621ce7a48fbde61232047561793748e6c9eba5af4d9a568570e40e0"
   revision 1
 
   depends_on "gmp" => :build
