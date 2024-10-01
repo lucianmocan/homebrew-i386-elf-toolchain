40eeace03c7db13517a9fcbda9bd18eb4320183d
Lucian Mocan <62904583+lucianmocan@users.noreply.github.com>
2024-10-01 10:05:54 +0200

    Update i386-elf-gcc.rb

--- Formula/i386-elf-gcc.rb
+++ Formula/i386-elf-gcc.rb
@@ -1,8 +1,8 @@
 class I386ElfGcc < Formula
   desc "GNU Compiler Collection targetting i386-elf"
   homepage "https://gcc.gnu.org"
-  url "ftp://ftp.irisa.fr/pub/mirrors/gcc.gnu.org/gcc/gcc-14.1.0/gcc-14.1.0.tar.xz"
-  sha256 "e283c654987afe3de9d8080bc0bd79534b5ca0d681a73a11ff2b5d3767426840"
+  url "https://mirror.linux-ia64.org/gnu/gcc/gcc-14.1.0/gcc-14.1.0.tar.xz"
+  sha512 "0c6ee313c336ebb028aac17ef60bac2dcf0b285f38c99916164c091506c9d56dedf60a96342de1e91e7c4eaf70d936cc0099c850dd5f148f425b05efb30f83eb"
   revision 1
 
   depends_on "gmp" => :build
